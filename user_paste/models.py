9662c1cbde2e997a05271eeb189e29fad1362d14
LoganHodgins <loganhodginsofficial@gmail.com>
2023-03-08 23:01:45 -0600

    added base app models

--- user_paste/models.py
+++ user_paste/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 
+class User(models.Model):
+    user_name = models.CharField(max_length=20)
+    # TODO: add password
+
+class Post(models.Model):
+    # TODO: add post url later
+    post_title = models.CharField(max_length=20)
+    post_author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    post_content = models.TextField(max_length=500)
+    post_description = models.CharField(max_length=20)
+    post_created_date = models.DateField(auto_now=True)
