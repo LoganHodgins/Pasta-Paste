16999ac112bb98cd5613605f638b9f939c005822
LoganHodgins <loganhodginsofficial@gmail.com>
2023-03-09 00:41:24 -0600

    added base form

--- user_paste/views.py
+++ user_paste/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 # Create your views here.
 
+from .forms import PostForm
+
 def index(req):
-    return render(req, 'user_paste/index.html')
+    return render(req, 'user_paste/index.html', {'PostForm': PostForm})
